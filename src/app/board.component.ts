2437845b06a63645b07c9f723a63156ab477ab55
cuiyongli <cuiyongli@ycmedia.com>
2018-08-24 16:15:03 +0800

    1

--- src/app/board.component.ts
+++ src/app/board.component.ts
@@ -1,23 +1,22 @@
 import {Component, ElementRef, OnInit, Renderer2, ViewChild,} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
-import {select, Store} from '@ngrx/store';
+import {Store} from '@ngrx/store';
 import {AppState} from '../store/model';
-import * as reducerMenu from '../store/reducer/menu.reducer';
 import {ScrollService} from "../service/scroll.service";
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.less']
 })
-export class BoardComponent implements OnInit{
-  @ViewChild('container') containerFullRef : ElementRef;
-  @ViewChild('picList') picList : ElementRef;
+export class BoardComponent implements OnInit {
+  @ViewChild('container') containerFullRef: ElementRef;
+  @ViewChild('picList') picList: ElementRef;
+
   constructor(private router: Router,
               private route: ActivatedRoute,
               private store: Store<AppState>,
-              private renderer:Renderer2,
-              private _scrollService:ScrollService,
-  ) {
+              private renderer: Renderer2,
+              private _scrollService: ScrollService,) {
     /*store.pipe(select(reducerMenu.getMenuState)).subscribe(menu => {
       if (menu.menuList && menu.menuList.length > 1) {
         router.navigate([menu.menuList[0].route], {queryParams: {...route.snapshot.queryParams}, replaceUrl: true});
@@ -27,53 +26,69 @@ export class BoardComponent implements OnInit{
 
   hash = 'home'
   leftTo;
-  background={
-    background:'rgba(255,255,255,0.7)'
+  background = {
+    background: 'rgba(255,255,255,0.7)'
   }
-  time = 3000
+  setIntervalNum
   currentPic = 0
+  num = 0
+  user: any = {}
+  flag: false;
   ngOnInit(): void {
     this.renderer.listen(this.containerFullRef.nativeElement, 'scroll', (event) => {
-        if(event.target.scrollTop > 180){
-          this.background={
-            background:'rgba(255,255,255,1)'
-          }
-        }else {
-          this.background={
-            background:'rgba(255,255,255,0.7)'
-          }
+      if (event.target.scrollTop > 180) {
+        this.background = {
+          background: 'rgba(255,255,255,1)'
         }
+      } else {
+        this.background = {
+          background: 'rgba(255,255,255,0.7)'
+        }
+      }
     })
-    setInterval(() => {
+    this.scrolls()
+    this.renderer.listen(this.picList.nativeElement, 'mouseenter', (event) => {
+      clearInterval(this.setIntervalNum)
+    })
+    this.renderer.listen(this.picList.nativeElement, 'mouseleave', (event) => {
+      this.scrolls()
+    })
+  }
+
+  scrolls() {
+    this.setIntervalNum = setInterval(() => {
       this.currentPic++
-      if(this.currentPic > 1){
+      if (this.currentPic > 1) {
         this.currentPic = 0
       }
-      this.leftTo={
+      this.leftTo = {
         'left.px': -this.currentPic * 1352
       }
-    },this.time)
-    this.renderer.listen(this.picList.nativeElement, 'mouseenter', (event) => {
-      console.info(event)
-      clearInterval(this.time)
-    })
-   }
-  num = 0
-  changeElement(index){
+    }, 3000)
+  }
+
+  changeElement(index) {
     this.num = index
   }
-  scrollTop(name){
+
+  scrollTop(name) {
     this.hash = name
-    this._scrollService.scrollTo(this.containerFullRef.nativeElement, {top:document.getElementById(name).offsetTop-80})
-  }
-  current(index){
-     this.currentPic = index
-     this.leftTo={
-        'left.px': -(index * 1352)
-     }
-  }
-  make(){
-
+    this._scrollService.scrollTo(this.containerFullRef.nativeElement, {top: document.getElementById(name).offsetTop - 80})
   }
 
+  current(index) {
+    this.currentPic = index
+    this.leftTo = {
+      'left.px': -(index * 1352)
+    }
+  }
+
+  make() {
+    if (!this.user.name || !this.user.phone) {
+      alert('不能为空（必填）')
+    }else {
+      this.flag = true
+    }
+
+  }
 }
